7545c7c30f4349c985cff972cd165a026fa5e2dd
Mike Greber <mikegreberdevelopment@gmail.com>
2022-08-24 11:11:43 -0600

    Add project files.

---
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Shared;
+
+public partial class ProductList
+{
+    [Inject] private HttpClient HttpClient { get; set; }
+
+    private static List<Product> Products = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await HttpClient.GetFromJsonAsync<List<Product>>("api/Product");
+       
+        if (result != null)
+            Products = result;
+    }
+}
